31d5bd522313387cb4b1047d4eb20589c12ea236
gmnutrizione <dott.murdaca.nutrizionista@gmail.com>
2026-09-03 10:56:56 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -5,13 +5,13 @@
 // Il link della scheda del singolo cliente invece NON va qui:
 // viene passato nell'indirizzo dell'app (parametro ?data=...)
 
-const ARCHIVIO_CSV_URL = "https://docs.google.com/spreadsheets/d/1M-6QGcdYz61884Q1AJQbD9ze361JqqXHNEjunUodZRk/gviz/tq?tqx=out:csv&gid=251396579";
+const ARCHIVIO_CSV_URL = "https://docs.google.com/spreadsheets/d/1M-6QGcdYz61884Q1AJQbD9ze361JqqXHNEjunUodZRk/export?format=csv&gid=251396579";
 
-const FOTO_CSV_URL = "https://docs.google.com/spreadsheets/d/1M-6QGcdYz61884Q1AJQbD9ze361JqqXHNEjunUodZRk/gviz/tq?tqx=out:csv&gid=572585223";
+const FOTO_CSV_URL = "https://docs.google.com/spreadsheets/d/1M-6QGcdYz61884Q1AJQbD9ze361JqqXHNEjunUodZRk/export?format=csv&gid=572585223";
 
 // Elenco clienti (Codice, Cliente, Link scheda) - usato per capire, dal codice
 // inserito, quale scheda caricare. Cos\u00ec il link dell'app resta uguale per tutti.
-const CLIENTI_CSV_URL = "https://docs.google.com/spreadsheets/d/1M-6QGcdYz61884Q1AJQbD9ze361JqqXHNEjunUodZRk/gviz/tq?tqx=out:csv&gid=1615113886";
+const CLIENTI_CSV_URL = "https://docs.google.com/spreadsheets/d/1M-6QGcdYz61884Q1AJQbD9ze361JqqXHNEjunUodZRk/export?format=csv&gid=1615113886";
 
 // Link dell'App web (Google Apps Script) che riceve kg, rep, commenti
 // e le modifiche alla scheda, scrivendoli nel tab "Risultati"
